f4dc0bdb956027f59ccfa642f212ecf832d11f9b
alfarisf1704-hub <alfarisf1704@gmail.com>
2026-05-11 16:24:17 +0700

    jantung h, manusia h

---
+++ jantung.h
@@ -0,0 +1,22 @@
+#ifndef JANTUNG_H
+#define JANTUNG_H
+
+using namespace std;
+
+class jantung
+{
+public:
+    jantung()
+    {
+        jantung()
+        {
+            cout << "jantung dihidupkan\n";
+        }
+
+        ~jantung()
+        {
+            cout << "jantung dimatikan\n";
+        }
+    }
+};
+#endif

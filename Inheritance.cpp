5326ce2c51d99821916c6a29efa790f85f9c3da9
alfarisf1704-hub <alfarisf1704@gmail.com>
2026-05-11 15:58:06 +0700

    membuat class pelajar

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -23,3 +23,14 @@ public:
         return a + b;
     }
 };
+
+class pelajar : public orang
+{
+public:
+    string sekolah;
+    pelajar(string pNama, string pSekolah) : orang(pNama), sekolah(pSekolah)
+    {
+        cout << "pelajar dibuat\n"
+             << endl;
+    }
+}

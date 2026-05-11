b2969e01017497989034757086cfb3248da04151
alfarisf1704-hub <alfarisf1704@gmail.com>
2026-05-11 15:43:12 +0700

    void beruntun dokter dan pasien

--- Asosiasi.cpp
+++ Asosiasi.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 class dokter;
-class paisen
+class pasien
 {
 public:
     string nama;
@@ -41,4 +41,29 @@ public:
 void pasien ::tambahDokter(dokter *pDokter)
 {
     daftar_dokter.push_back(pDokter);
+}
+
+void pasien ::cetakDokter()
+{
+    cout << "Daftar dokter yang menangani pasien \"" << this->nama << "\":\n";
+    for (auto &a : daftar_dokter)
+    {
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
+void dokter ::tambahPasien(pasien *pPasien)
+{
+    daftar_pasien.push_back(pPasien);
+    pPasien->tambahDokter(this);
+}
+
+void dokter::cetakPasien()
+{
+    cout << "Daftar Pasien dari dokter\"" << this->nama << "\":\n";
+    for (auto &a : daftar_pasien)
+    {
+        cout << a->nama << "\n";
+    }
+    cout << endl;
 }
